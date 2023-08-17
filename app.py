0caa3bdf82f33670d59fca5d8befbb376a11e21c
Vidhi <vidhigupta1024@gmail.com>
2023-08-17 14:15:54 +0530

    -r

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 # import pickle
 import numpy as np
 import joblib
+import warnings
 # import sklearn
 # from sklearn.preprocessing import LabelEncoder
 from sklearn.exceptions import InconsistentVersionWarning
